f1e402b8acfa1489ebcac59768df60b80b56d5e5
kimberly <kimberly.lantay.wright@gmail.com>
2023-03-06 15:33:18 -0500

    send to commit

--- model.py
+++ model.py
@@ -55,15 +55,15 @@ def convert(prediction_list):
         output[date] = data["trend"]
     return output
 
-"""
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Predict')
     parser.add_argument('--ticker', type=str, default='MSFT', help='Stock Ticker')
     parser.add_argument('--days', type=int, default=7, help='Number of days to predict')
     args = parser.parse_args()
- """       
+      
     train(args.ticker)
     prediction_list = predict(ticker=args.ticker, days=args.days)
     output = convert(prediction_list)
     print(output)
-    
+   
