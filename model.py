a1708985210b8d38db130456321273dbcabaa100
kimberly <kimberly.lantay.wright@gmail.com>
2023-03-06 15:33:07 -0500

    send to commit

--- model.py
+++ model.py
@@ -43,8 +43,8 @@ def predict(ticker="MSFT", days=7):
 
     forecast = model.predict(df)
 
-    model.plot(forecast).savefig(f"{ticker}_plot.png")
-    model.plot_components(forecast).savefig(f"{ticker}_plot_components.png")
+   # model.plot(forecast).savefig(f"{ticker}_plot.png")
+   # model.plot_components(forecast).savefig(f"{ticker}_plot_components.png")
     
     return forecast.tail(days).to_dict("records")
 
@@ -55,13 +55,13 @@ def convert(prediction_list):
         output[date] = data["trend"]
     return output
 
-
+"""
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Predict')
     parser.add_argument('--ticker', type=str, default='MSFT', help='Stock Ticker')
     parser.add_argument('--days', type=int, default=7, help='Number of days to predict')
     args = parser.parse_args()
-        
+ """       
     train(args.ticker)
     prediction_list = predict(ticker=args.ticker, days=args.days)
     output = convert(prediction_list)
